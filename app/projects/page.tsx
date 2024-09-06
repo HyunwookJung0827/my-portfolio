3262f150c361b48c739fe4d90d4a94b23b94283f
HyunwookJung0827 <as9804@berkeley.edu>
2024-09-06 11:28:19 -0700

    Added cloudinary for image uploads

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 
 const Projects = () => {
   const [projects, setProjects] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchProjects = async () => {
@@ -15,6 +16,8 @@ const Projects = () => {
         setProjects(data);
       } catch (error) {
         console.error("Error fetching projects:", error);
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -24,25 +27,32 @@ const Projects = () => {
   return (
     <Container>
       <h1 className="text-6xl m-6">Projects</h1>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-        {projects.map((project) => (
-          <ProjectCard key={project._id} project={project} />
-        ))}
-      </div>
+      {loading ? (
+        <div className="flex gap-4">
+          <p className="text-center text-xl text-gray-500 animate-pulse">Loading...</p>
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+          {projects.map((project) => (
+            <ProjectCard key={project._id} project={project} />
+          ))}
+        </div>
+      )}
     </Container>
   );
 };
 
 const ProjectCard = ({ project }: { project: any }) => (
-  <div className="project-card">
-    <h3>{project.title}</h3>
-    <p>{project.description}</p>
+  <div className="project-card bg-white p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
+    <h3 className="text-2xl font-bold mb-2">{project.title}</h3>
+    <p className="text-gray-700 mb-4">{project.description}</p>
     {project.imageUrl && (
       <Image
         src={project.imageUrl}
         alt={project.title}
         width={560}
         height={315}
+        className="rounded-lg"
       />
     )}
     {project.videoUrl && (
@@ -53,14 +63,23 @@ const ProjectCard = ({ project }: { project: any }) => (
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
+        className="rounded-lg"
       ></iframe>
     )}
     {project.link && (
-      <a href={project.link} target="_blank">
+      <a
+        href={project.link}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="inline-block bg-blue-500 text-white text-lg font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-blue-600 transition duration-300 my-2"
+      >
         View Project
       </a>
     )}
-    <div>Tech Stack: {project.techStack.join(", ")}</div>
+    <div className="text-gray-600">
+      <span className="font-semibold">Tech Stack:</span>{" "}
+      {project.techStack.join(", ")}
+    </div>
   </div>
 );
 
