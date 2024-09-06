69057e317fe3b1648ea6d144b80e9590e975297d
HyunwookJung0827 <as9804@berkeley.edu>
2024-09-06 13:04:25 -0700

    ContactBox scale up styling

--- app/components/ContactBox.tsx
+++ app/components/ContactBox.tsx
@@ -1,11 +1,10 @@
 "use client";
 
 import Link from "next/link";
-import Button from "./Button";
 
 const ContactBox = () => {
   return (
-      <Link href="mailto:as9804@berkeley.edu" className="bg-slate-700 text-white py-4 px-8 w-96 h-80 rounded-lg text-center justify-center place-content-center hover:bg-blue-700 transition duration-300">
+      <Link href="mailto:as9804@berkeley.edu" className="bg-slate-700 text-white py-4 px-8 w-96 h-80 rounded-lg text-center justify-center place-content-center hover:bg-blue-700 transition duration-300 hover:scale-105">
         <p className="text-5xl font-bold my-2">Click Me</p>
         <p className="text-2xl">as9804@berkeley.edu</p>
       </Link >
